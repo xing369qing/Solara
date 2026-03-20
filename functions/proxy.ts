7fb22f7782dd868441d8e2bca6ec012b8a56d807
xing369qing <jack666chao@gmail.com>
2026-03-20 18:35:52 +0800

    Change API base URL for music source

--- functions/proxy.ts
+++ functions/proxy.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://music-api.gdstudio.xyz/api.php";
+const API_BASE_URL = "https://raw.githubusercontent.com/pdone/lx-music-source/main/sixyin/latest.js";
 const KUWO_HOST_PATTERN = /(^|\.)kuwo\.cn$/i;
 const SAFE_RESPONSE_HEADERS = ["content-type", "cache-control", "accept-ranges", "content-length", "content-range", "etag", "last-modified", "expires"];
 
